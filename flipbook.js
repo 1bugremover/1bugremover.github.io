5f95da484fdd28538366390d97a21608f3d1426c
1bugremover <anjaliamit@Anjalis-MacBook-Air.local>
2018-05-03 21:54:46 -0700

    changes and corrections

---
+++ flipbook.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+
+<html lang="en">
+<head>
+    
+    
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    <link rel="stylesheet" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script type="text/javascript">
+	$("#flipbook").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
+</script>
+
+    
+
+
+
+    
+    
+  </head>
+
+  <body>
+
+
+
+
+
+<div id="flipbook">
+	<div class="hard"> Turn.js </div>
+	<div class="hard"></div>
+	<div> Page 1 </div>
+	<div> Page 2 </div>
+	<div> Page 3 </div>
+	<div> Page 4 </div>
+	<div class="hard"></div>
+	<div class="hard"></div>
+</div>
+
+</body>
